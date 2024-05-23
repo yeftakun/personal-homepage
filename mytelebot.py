84bab2b6446b04751fdb676997ed05bed5d271bf
yeftakun <yeftakun34@gmail.com>
2024-05-23 17:57:54 +0800

    update token

--- mytelebot.py
+++ mytelebot.py
@@ -2,14 +2,12 @@ import telebot
 
 # Ganti TOKEN_BOT_ANDA dengan token bot Telegram Anda
 TOKEN = '7095664056:AAEiPyzvnPYcWBk2SVNgfYHk0TgRWaa-g4c'
-
 bot = telebot.TeleBot(TOKEN)
 
 # Menangani perintah /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Halo")
-    print("Pesan terkirim")
+    bot.reply_to(message, "Ok")
 
 # Menangani pesan yang diterima
 @bot.message_handler(func=lambda _: True)
